8aa97335f119ef4bb92411e85d8f85cb2befcde3
Javier Melguizo <melguizojavier@gmail.com>
2021-10-08 17:20:06 +0200

    Home

---
+++ src/jmelguizo/RunnerOne.java
@@ -0,0 +1,12 @@
+package jmelguizo;
+
+public class RunnerOne {
+
+    public RunnerOne(){
+        System.out.println("Runner One!");
+
+    }
+    public static void main(String[] args) throws Exception {
+        RunnerOne runnerOne = new RunnerOne();
+    }
+}
